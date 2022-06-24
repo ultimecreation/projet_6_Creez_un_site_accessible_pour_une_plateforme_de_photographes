ee87f847340292d61c1c053c1f5b3a77c1cc3a2d
ultimecreation <naserbechata@gmail.com>
2022-06-24 20:35:57 +0200

    added images+ updated photographer article

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,16 +1,55 @@
 function photographerFactory(data) {
-    const { name, portrait } = data;
+    const { id, name, city, country, tagline, price, portrait } = data;
 
     const picture = `assets/photographers/${portrait}`;
 
     function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
+        // create article tag
+        const article = document.createElement('article');
+
+        // create img tag and set attribute
+        const img = document.createElement('img');
         img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
+
+        // create h2
+        const h2 = document.createElement('h2');
         h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
+
+        // create a tag and set attributes
+        const a = document.createElement('a')
+        a.setAttribute('href',`photographer.html?id=${id}`)
+        a.setAttribute('focusable', true)
+        a.setAttribute('aria-label', name)
+
+        // create p tags,set attributes and text content
+        const pLocation = document.createElement('p')
+        pLocation.textContent = `${city}, ${country}`
+        const pTagline = document.createElement('p')
+        pTagline.textContent = tagline
+        const pPrice = document.createElement('p')
+        pPrice.textContent = `${price}€/jour`
+
+        // create ul tag and set attribute
+        const ul = document.createElement('ul')
+        // ul.setAttribute('aria-label',`${name} summary`)
+
+        // create li tags
+        const liLocation = document.createElement('li')
+        const liTagline = document.createElement('li')
+        const liPrice = document.createElement('li')
+
+        // create article dom tree
+        a.appendChild(img)
+        a.appendChild(h2)
+        liLocation.appendChild(pLocation);
+        liTagline.appendChild(pTagline);
+        liPrice.appendChild(pPrice);
+        ul.appendChild(liLocation)
+        ul.appendChild(liTagline);
+        ul.appendChild(liPrice)
+        article.appendChild(a);
+        article.appendChild(ul);
+        
         return (article);
     }
     return { name, picture, getUserCardDOM }
--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,28 +1,11 @@
     async function getPhotographers() {
         // Penser à remplacer par les données récupérées dans le json
-        const photographers = [
-            {
-                "name": "Ma data test",
-                "id": 1,
-                "city": "Paris",
-                "country": "France",
-                "tagline": "Ceci est ma data test",
-                "price": 400,
-                "portrait": "account.png"
-            },
-            {
-                "name": "Autre data test",
-                "id": 2,
-                "city": "Londres",
-                "country": "UK",
-                "tagline": "Ceci est ma data test 2",
-                "price": 500,
-                "portrait": "account.png"
-            },
-        ]
+        const data = await(await fetch('../../../data/photographers.json')).json()
+        const {photographers} = data
+        
         // et bien retourner le tableau photographers seulement une fois
         return ({
-            photographers: [...photographers, ...photographers, ...photographers]})
+            photographers: [...photographers]})
     }
 
     async function displayData(photographers) {
@@ -39,6 +22,7 @@
         // Récupère les datas des photographes
         const { photographers } = await getPhotographers();
         displayData(photographers);
+        
     };
     
     init();
