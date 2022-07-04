19c7c391472d86014b6ef6d2b3400442a6ba8e80
ultimecreation <naserbechata@gmail.com>
2022-07-04 20:02:55 +0200

    added close lightbox on sort click

--- scripts/utils/dropdown.js
+++ scripts/utils/dropdown.js
@@ -1,6 +1,9 @@
 const dropdown = document.querySelector('.dropdown-menu')
-
 const handleSort = (e) => {
+    // close light if opened to avoid conflict
+    if(document.querySelector('#lightbox_modal').style.display === 'block'){
+        closeLightboxModal()
+    }
     const sortBy = e.target.closest('[data-value]').dataset.value
     const urlParams = new URLSearchParams(window.location.search)
     const userId = urlParams.get('id')
